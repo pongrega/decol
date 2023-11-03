58f1b6701f7aa8b752a666247a6b18f8b439c165
pongrega <149714497+pongrega@users.noreply.github.com>
2023-11-03 19:03:03 +0500

    Add files via upload

---
+++ wp-content/plugins/otter-blocks/build/animation/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives', 'wp-rich-text'), 'version' => '0b068fedd24031dc01b8');
